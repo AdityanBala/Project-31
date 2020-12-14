1cd5a3b4d99764598f101c1e9ada1ed57be0f26b
AdityanBala <74098286+AdityanBala@users.noreply.github.com>
2020-12-14 17:38:21 +0530

    Add files via upload

---
+++ particle.js
@@ -0,0 +1,26 @@
+class Particle{
+    constructor(x,y,r){
+     var options={
+         isStatic:false,
+         restitution:0.3
+     }
+     this.r = r;
+     this.body = Bodies.circle(x,y,r,options);
+     this.color = color(random(0,255),random(0,255),random(0,255));
+     World.add(world,this.body);
+
+
+
+    }
+   display(){
+     var pos = this.body.position;
+       
+     push();
+     translate(pos.x,pos.y);
+     fill(this.color);
+     ellipseMode(RADIUS);
+     ellipse(0,0,this.r,this.r);
+      
+     pop(); 
+   }
+}
